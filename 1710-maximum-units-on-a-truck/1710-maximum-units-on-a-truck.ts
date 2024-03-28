0a9c0670b1db2ac147f44945239da4c1872a3c76
Tyrrhene <164898638+Tyrrhene@users.noreply.github.com>
2024-03-28 18:12:06 +0000

    Time: 71 ms (66.29%), Space: 54.5 MB (34.83%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.ts
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.ts
@@ -3,13 +3,14 @@ function maximumUnits(boxTypes: number[][], truckSize: number): number {
     
     let totalUnits = 0;
     
-    for (const box of boxTypes) {
-        const boxesCount = Math.min(box[0], truckSize);
-        totalUnits += boxesCount * box[1];
+    for (const [numberOfBoxes, unitsPerBox] of boxTypes) {
+        const boxesToLoad = Math.min(numberOfBoxes, truckSize);
+        totalUnits += boxesToLoad * unitsPerBox;
+        truckSize -= boxesToLoad;
         
-        truckSize -= boxesCount;
-        
-        if (truckSize === 0) break;
+        if (truckSize === 0) {
+            break;
+        }
     }
     
     return totalUnits;
