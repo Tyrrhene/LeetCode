40d9d6f4944b218b8242e8f6f14a20bcf55f6817
Matt <164898638+Tyrrhene@users.noreply.github.com>
2024-04-16 13:05:57 +0100

    Time: 62 ms (30.21%), Space: 51.1 MB (73.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,4 +1,4 @@
 function plusOne(digits: number[]): number[] {
-    const number = BigInt(digits.join('')) + BigInt(1); // Using BigInt to handle large numbers
-    return Array.from(String(number), (digit, index) => index === 0 ? digit : Number(digit)) as number[];
+    const number = BigInt(digits.join('')) + BigInt(1);
+    return Array.from(number.toString()).map(Number);
 }
