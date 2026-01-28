b5caabdbe64c511ba73cd679ffb145312aea9295
Matt <164898638+Tyrrhene@users.noreply.github.com>
2026-01-28 10:39:58 +0000

    Time: 12 ms (21.89%), Space: 66 MB (13.92%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,14 +1,8 @@
 function isPalindrome(x: number): boolean {
-    const string = x.toString();
-    if (string.length === 1) {
-        return true;
-    }
-    
-    const len = Math.floor(string.length / 2);
-    for (let i = 0; i < len; i++) {
-        if (string[i] !== string[string.length - i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
+   let reverse =  x.toString().split("").reverse().join("");
+   if (x === parseInt(reverse)) {
+    return true
+   } else {
+    return false
+   }
+};
