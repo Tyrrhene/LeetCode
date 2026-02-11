c1fab70a3c81afc41f741411bc851f72d5447d0f
Matt <164898638+Tyrrhene@users.noreply.github.com>
2026-02-11 22:53:07 +0000

    Time: 23 ms (28.67%), Space: 73.9 MB (85.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.ts
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -1,9 +1,3 @@
 function containsDuplicate(nums: number[]): boolean {
-  const alreadyExists = new Set<number>();
-
-  for (const number of nums) {
-    if (alreadyExists.has(number)) return true;
-    alreadyExists.add(number);
-  }
-  return false;
-}
+    return nums.length !== new Set(nums).size;
+};
